15b8cdb21a830979fc48e86597fa4b267ccc2703
ejazsayyad <ejazsayyad4@gmail.com>
2020-04-19 17:49:33 +0530

    Updates

---
+++ precache-manifest.0d395b0d7693a96b02727159356b11ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a647e8ec94e3b11b2f6129f77f0b46f7",
+    "url": "/CommentGenerator/index.html"
+  },
+  {
+    "revision": "ef4a900854b94dbeef38",
+    "url": "/CommentGenerator/static/css/main.c55bd31b.chunk.css"
+  },
+  {
+    "revision": "3429b52462fde4f7e621",
+    "url": "/CommentGenerator/static/js/2.1d61e765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CommentGenerator/static/js/2.1d61e765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef4a900854b94dbeef38",
+    "url": "/CommentGenerator/static/js/main.7cfcec0a.chunk.js"
+  },
+  {
+    "revision": "99f4d30e82b47b83483c",
+    "url": "/CommentGenerator/static/js/runtime-main.d8a4bc1f.js"
+  }
+]);
